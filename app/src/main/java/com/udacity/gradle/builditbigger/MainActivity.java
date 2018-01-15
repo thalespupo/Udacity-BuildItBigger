b1fb0cd90a0f9967121eacf08b9f5effe28a1130
Thales Pupo Araújo <thalespupo@gmail.com>
2018-01-15 17:23:37 -0200

    Fix for some bugs found in review

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.tapura.badjokes.BadJokesMan;
-import com.tapura.bigbadjokes.BadJokesActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,10 +39,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, BadJokesActivity.class);
-        intent.putExtra(BadJokesActivity.JOKE, BadJokesMan.getJoke());
-        startActivity(intent);
+        new EndpointAsyncTask().execute(this);
     }
-
-
 }
